ae8810902c79e8cb6b3f561fcb3dbf2a7427c3a3
Eric Kersten <ekersten@gmail.com>
2017-11-23 10:26:59 -0300

    router changes and cleanup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import ReactRouterApp from './components/ReactRouterApp';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+const ExpenseDashboardPage = () => (
+    <div>
+    This is from my dashboard component
+    </div>
+)
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+)
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+)
+
+const HelpPage = () => (
+    <div>
+        This is from my help page component
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact="true" />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+        
+    </BrowserRouter>
+)
+    
+
+
+ReactDOM.render(routes, document.getElementById('app'));
---
+++ src/components/ReactRouterApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ReactRouterApp extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>React Router App</h1>
+            </div>
+        );
+    }
+
+}
