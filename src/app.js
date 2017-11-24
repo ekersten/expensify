fa6075c89e850a24eb57831875b112864b82cf1f
Eric Kersten <ekersten@gmail.com>
2017-11-24 15:46:21 -0300

    expenses filter and sort

--- src/app.js
+++ src/app.js
@@ -12,13 +12,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', ammount: 45000 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-}, 3000);
+store.dispatch(addExpense({ description: 'Water bill', ammount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', ammount: 109500,  }));
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, ammount, createdAt}) => (
+const ExpenseListItem = ({ id, description, ammount, createdAt, dispatch}) => (
     <div>
         <h3>{ description }</h3>
         <p>{ ammount } - { createdAt }</p>
+        <p>{ id }</p>
+        <button
+            onClick={() => {
+                dispatch(removeExpense({id}));
+            }}
+        >Remove</button>
     </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
