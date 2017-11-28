79cd1fb0b59ba0242119cf207f5e06df66a884ac
Eric Kersten <ekersten@gmail.com>
2017-11-28 14:47:23 -0300

    webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                //resolve-url-loader may be chained before sass-loader if necessary
-                use: ['css-loader', 'sass-loader']
-            })
-        }]
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css')
-    ],
-    devtool: 'nosources-source-map',
-    'devServer': {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        'devServer': {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-
-}
+};
